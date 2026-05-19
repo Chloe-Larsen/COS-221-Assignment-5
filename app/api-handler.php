b69d48209260f99e3e701eb46de6a4e6059fb5f7
Chloe Larsen <chloelarsen72@gmail.com>
2026-05-19 13:33:31 +0200

    Moved files and changed directories

--- app/api-handler.php
+++ app/api-handler.php
@@ -6,9 +6,10 @@
 
 $routes = [
     '/api/test' => 'api/test.php',
+    'api/login' => 'api/login'
 ];
 
 if (array_key_exists($currentRoute, $routes)) {
-    $targetFile = __DIR__ . "/../app/" . $routes[$currentRoute];
+    $targetFile = __DIR__ . "/" . $routes[$currentRoute];
     require_once $targetFile;
 }
