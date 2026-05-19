62b3155fbef90e9f7197ec5f9f3d936c14bed865
Chloe Larsen <chloelarsen72@gmail.com>
2026-05-19 18:41:58 +0200

    Flight endpoint

--- app/api/flight.php
+++ app/api/flight.php
@@ -2,16 +2,16 @@
 if ($method === "GET") {
     if (isset($_GET['id'])) {
         $query = "SELECT * FROM Flight WHERE flightId = :id";
-        $destination = $db->fetch($query, [':id' => $_GET['id']]);
+        $flight = $db->fetch($query, [':id' => $_GET['id']]);
 
-        if ($destination) {
-            sendRes(200, ["message" => "Success", "data" => $destination]);
+        if ($flight) {
+            sendRes(200, ["message" => "Success", "data" => $flight]);
         } else {
             sendRes(404, ["message" => "Destination not found."]);
         }
     } else {
         $query = "SELECT * FROM Flight";
-        $accommodations = $db->fetchAll($query);
-        sendRes(200, ["message" => "Success", "data" => $accommodations]);
+        $flight = $db->fetchAll($query);
+        sendRes(200, ["message" => "Success", "data" => $flight]);
     }
 }
